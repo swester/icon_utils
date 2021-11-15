68477025ff2a4ee30a2aef3b086fdeb768e6f6d2
Stephanie Westerhuis <stephanie.westerhuis@usys.ethz.ch>
2021-11-15 10:22:45 +0100

    changed docstring of latlonindex fct

--- toolbox.py
+++ toolbox.py
@@ -3,19 +3,18 @@
 import numpy as np
 
 def ind_from_latlon(lats,lons,lat,lon,verbose=False):
-    '''
-    find the nearest neighbouring index to given location
+    """Find the nearest neighbouring index to given location.
 
-    input:
-    lats    2d array
-    lons    2d array
-    lat     float
-    lon     float
+    Args:
+        lats (2d array):            Latitude grid
+        lons (2d array):            Longitude grid
+        lat (float):                Latitude of location   
+        lon (float):                Longitude of location 
+        verbose (bool, optional):   Print information. Defaults to False.
 
-    output:
-    ind     integer
-    '''
-
+    Returns:
+        int     Index of nearest grid point.
+    """
     dist = [np.sqrt((lats[i]-lat)**2 + (lons[i]-lon)**2) for i in range(len(lats))]
     ind = np.where(dist==np.min(dist))[0][0]
 
