fbc8c2f382b26bf68560838f99589562348b6d45
Thomas <Thomas.Toschini@meteoswiss.ch>
2021-11-19 19:03:27 +0100

    added variables and attribute

--- variable_dataframe.py
+++ variable_dataframe.py
@@ -3,10 +3,10 @@ import pandas as pd
 vdf = pd.DataFrame(
         # variables
         columns = ['lw_net', 'lw_down', 'lw_up', 'tqc', 'sw_net', 'latent_heat', 'sensible_heat',
-                   'temp', 'qc', 'qv', ],
+                   'temp', 'qc', 'qv','Ts', 'Ws'],
         # attributes
         index = ['var_name', 'long_name', 'short_name',
-                 'unit', 'min_value', 'max_value', 
+                 'unit', 'min_value', 'max_value', 'var_id'
                  # line appearance
                  'marker', 'linestyle', 'color',
                  # value transformations
@@ -42,6 +42,7 @@ vdf['lw_down'].max_value   = 10
 vdf['lw_down'].color       = 'red'
 vdf['lw_down'].marker      = 'o'
 vdf['lw_down'].avg         = True
+vdf['lw_down'].var_id      = '175'
 
 vdf['lw_up'].var_name    = 'ATHU_S'
 vdf['lw_up'].long_name   = 'Upward longwave radiation'
@@ -52,6 +53,8 @@ vdf['lw_up'].max_value   = 10
 vdf['lw_up'].color       = 'orange'
 vdf['lw_up'].marker      = 'o'
 vdf['lw_up'].avg         = True
+vdf['lw_up'].mult        = (-1)
+vdf['lw_up'].var_id      = '1531'
 
 vdf['sw_net'].var_name    = 'ASOB_S'
 vdf['sw_net'].long_name   = 'Net shortwave radiation'
@@ -71,7 +74,6 @@ vdf['latent_heat'].min_value   = -200
 vdf['latent_heat'].max_value   = 50
 vdf['latent_heat'].color       = 'pink'
 vdf['latent_heat'].marker      = 'o'
-vdf['latent_heat'].mult        = 1
 vdf['latent_heat'].avg         = True 
 
 vdf['sensible_heat'].var_name    = 'ASHFL_S'
@@ -82,9 +84,8 @@ vdf['sensible_heat'].min_value   = -200
 vdf['sensible_heat'].max_value   = 50
 vdf['sensible_heat'].color       = 'yellow'
 vdf['sensible_heat'].marker      = 'o'
-vdf['sensible_heat'].mult        = 1
 vdf['sensible_heat'].avg         = True 
-
+vdf['sensible_heat'].var_id      = '2786' #not sure...
 
 vdf['tqc'].var_name    = 'TQC'
 vdf['tqc'].long_name   = 'Liquid water path'
@@ -92,5 +93,26 @@ vdf['tqc'].unit        = 'kg m-2'
 vdf['tqc'].short_name  = 'LWP'
 vdf['tqc'].min_value   = 0
 vdf['tqc'].max_value   = 0.05
-vdf['tqc'].color       = 'darkblue'
+vdf['tqc'].color       = 'green'
 vdf['tqc'].marker      = 'o'
+vdf['tqc'].var_id      = '5547'
+
+vdf['Ts'].var_name    = 'T_G'
+vdf['Ts'].long_name   = 'Temperature at the ground-atm interface'
+vdf['Ts'].unit        = 'K'
+vdf['Ts'].short_name  = 'Ts'
+vdf['Ts'].min_value   = 270
+vdf['Ts'].max_value   = 290
+vdf['Ts'].color       = 'gray'
+vdf['Ts'].marker      = 'o'
+vdf['Ts'].var_id      = '201'
+
+vdf['Ws'].var_name    = 'W_i'
+vdf['Ws'].long_name   = 'Interception storage water'
+vdf['Ws'].unit        = 'g Kg-1'
+vdf['Ws'].short_name  = 'Ws'
+vdf['Ws'].min_value   = 0
+vdf['Ws'].max_value   = 0.25
+vdf['Ws'].color       = 'black'
+vdf['Ws'].marker      = 'o'
+vdf['Ws'].mult        = 1000
